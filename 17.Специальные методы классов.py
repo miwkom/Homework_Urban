93f191af0eeb897d9c8bbafe5e69611764e36674
miwkom <166976262+miwkom@users.noreply.github.com>
2024-06-04 21:41:54 +0300

    Add files via upload

--- 17.Специальные методы классов.py
+++ 17.Специальные методы классов.py
@@ -1,28 +1,13 @@
 class House:
-    def __init__(self, address, number_of_floor):
-        self.name = address
-        self.number_of_floor = number_of_floor
+    def __init__(self):
+        self.numberOfFloors = 0
 
-    def go_to(self, new_floor):
-        if new_floor > self.number_of_floor:
-            print("Такого этажа не существует")
-        else:
-            for i in range(new_floor):
-                print(i + 1)
-
-    def set_new_number_of_floor(self, floor):
-        self.number_of_floor = floor
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print("Новое количество этажей:", self.numberOfFloors)
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
+h1 = House()
 
-print(h1.number_of_floor)
-print(h2.number_of_floor)
-print()
-
-h1.set_new_number_of_floor(5)
-h2.set_new_number_of_floor(10)
-
-print(h1.number_of_floor)
-print(h2.number_of_floor)
+h1.setNewNumberOfFloors(3)
+h1.setNewNumberOfFloors(10)
