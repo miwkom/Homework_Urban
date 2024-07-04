514cc7a256f94470f61086f436a391e91065d7e2
miwkom <166976262+miwkom@users.noreply.github.com>
2024-07-04 23:57:21 +0300

    Add files via upload

--- 21.Доступ к свойствам родителя. Переопределение свойств.py
+++ 21.Доступ к свойствам родителя. Переопределение свойств.py
@@ -1,24 +1,25 @@
 class Vehicle:
+    __COLOR_VARIANTS = ['black', 'white', 'blue', 'green']
+
     def __init__(self, owner, model, color, engine_power):
         self.owner = str(owner)
         self.__model = str(model)
         self.__engine_power = int(engine_power)
         self.__color = str(color)
-        self.__COLOR_VARIANTS = ['black', 'white', 'blue', 'green']
 
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        return f"Модель: {self.__model}"
 
     def get_horsepower(self):
-        print(f'Мощность двигателя: {self.__engine_power}')
+        return f"Мощность двигателя: {self.__engine_power}"
 
     def get_color(self):
-        print(f'Цвет: {self.__color}')
+        return f"Цвет: {self.__color}"
 
     def print_info(self):
-        self.get_model()
-        self.get_horsepower()
-        self.get_color()
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
         print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
@@ -38,3 +39,4 @@ vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
 vehicle1.print_info()
+vehicle1.get_color()
