cc8a9a0faa8de4cab15f5fd776c45c075d9b3273
miwkom <166976262+miwkom@users.noreply.github.com>
2024-07-16 02:43:05 +0300

    Add files via upload

---
+++ 36.Генераторы.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    n = len(text)
+    for i in range(1, n+1):
+        for j in range(n-i+1):
+            yield text[j:j+i]
+
+
+a = all_variants("abc")
+for g in a:
+    print(g)
