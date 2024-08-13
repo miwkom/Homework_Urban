8a42882b59492c29dbb15d86eb9fb184b82dfe5e
miwkom <166976262+miwkom@users.noreply.github.com>
2024-08-13 14:48:35 +0300

    Update 42.Многопроцессное программирование.py

--- 42.Многопроцессное программирование.py
+++ 42.Многопроцессное программирование.py
@@ -11,35 +11,30 @@ class WarehouseManager:
         if action == "receipt":
             if product in self.data:
                 self.data[product] += quantity
+                print(product)
             else:
                 self.data[product] = quantity
         elif action == "shipment":
             if product in self.data and self.data[product] > 0:
                 self.data[product] -= quantity
-                if self.data[product] == 0:
+                if self.data[product] <= 0:
                     del self.data[product]
 
     def run(self, requests):
-        for request in requests:
-            p = multiprocessing.Process(target=self.process_request, args=(request,))
+        for req in requests:
+            p = multiprocessing.Process(target=self.process_request, args=(req,))
             p.start()
             p.join()
 
 
-# Создаем менеджера склада
-manager = WarehouseManager()
-
-# Множество запросов на изменение данных о складских запасах
-requests = [
-    ("product1", "receipt", 100),
-    ("product2", "receipt", 150),
-    ("product1", "shipment", 30),
-    ("product3", "receipt", 200),
-    ("product2", "shipment", 50),
-]
-
-# Запускаем обработку запросов
-manager.run(requests)
-
-# Выводим обновленные данные о складских запасах
-print(manager.data)
+if __name__ == '__main__':
+    manager = WarehouseManager()
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50),
+    ]
+    manager.run(requests)
+    print(manager.data)
