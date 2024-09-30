5f3677a5128dc884701c93322dd877a734aad7e8
miwkom <166976262+miwkom@users.noreply.github.com>
2024-09-30 16:34:33 +0300

    Add files via upload

---
+++ Основы_Fast_Api_и_маршрутизация.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome():
+    return {'message': 'Главная страница'}
+
+
+@app.get("/user/admin")
+async def admin():
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def user(user_id):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user")
+async def edit_user(username, age):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
